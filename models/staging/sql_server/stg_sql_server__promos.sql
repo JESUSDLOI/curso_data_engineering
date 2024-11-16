f1cc205524c6646c55871721fb1a5c6a0b89c56f
Jesus de la Oliva Iglesias <jesus.delaoliva.ig@gmail.com>
2024-11-16 11:51:28 +0000

    Creo modelo de fecha y comienzo promos

--- models/staging/sql_server/stg_sql_server__promos.sql
+++ models/staging/sql_server/stg_sql_server__promos.sql
@@ -1,5 +1,23 @@
-{{ codegen.generate_base_model(
-    source_name='sql_server_dbo',
-    table_name='promos',
-    materialized='view'
-) }}
+{{ config(materialized='view') }}
+
+with source as (
+
+    select * from {{ source('sql_server_dbo', 'promos') }}
+
+),
+
+renamed as (
+
+    select
+        md5(trim(promo_id)) as promo_id,
+        promo_id as promo_name,
+        discount,
+        status,
+        _fivetran_deleted,
+        _fivetran_synced at date_load
+
+    from source
+
+)
+
+select * from renamed
---
+++ models/staging/sql_server/stg_sql_server__time.sql
@@ -0,0 +1,6 @@
+{{
+    config(
+        materialized = "table"
+    )
+}}
+{{ dbt_date.get_date_dimension('2000-01-01', '2030-12-31') }}
