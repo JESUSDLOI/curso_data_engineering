6377fa237aca481efbc2ee54332c828936d722db
JESUSDLOI <130898630+JESUSDLOI@users.noreply.github.com>
2025-05-20 11:57:19 +0200

    Update crear_procedure.sql

--- insert_info.sql/crear_procedure.sql
+++ insert_info.sql/crear_procedure.sql
@@ -95,7 +95,7 @@ BEGIN
     EXECUTE IMMEDIATE '
         CREATE OR REPLACE TEMPORARY TABLE temp_transformacion_user_fct_' || env || ' AS (
         
-            WITH user_fact AS (                      
+                                
             SELECT
                 *
             FROM temp_delta_user_fct_' || env || ' a
