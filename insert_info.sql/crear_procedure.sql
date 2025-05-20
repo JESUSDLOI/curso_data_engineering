080b03792b84661c904c6d7ec6ed9125c7e74195
JESUSDLOI <130898630+JESUSDLOI@users.noreply.github.com>
2025-05-20 11:58:55 +0200

    Update crear_procedure.sql

--- insert_info.sql/crear_procedure.sql
+++ insert_info.sql/crear_procedure.sql
@@ -182,8 +182,7 @@ BEGIN
     result := (
         SELECT 
             a."number of rows inserted" AS "Filas insertadas",
-            a."number of rows updated" AS "Filas actualizadas",
-            b."number of rows deleted" AS "Filas eliminadas"                
+            a."number of rows updated" AS "Filas actualizadas"             
         FROM TABLE(RESULT_SCAN(:merge_query_id)) a
         FULL OUTER JOIN TABLE(RESULT_SCAN(:delete_query_id)) b
         ON 1 = 1
